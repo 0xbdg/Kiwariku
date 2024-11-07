176b64c452c1cac832e71b5e682e6eac72f68ec5
?0 <benjamindguntara@protonmail.com>
2024-11-07 18:17:25 +0700

    sdfsdjfi

--- kiwariku/settings.py
+++ kiwariku/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['103.151.140.206','situsari.desa.id', 'localhost', '127.0.0.1']
 
 
 # Application definition
