d10670c2c4237b94d552e5a735de2ca95d172fff
?0 <benjamindguntara@protonmail.com>
2024-12-04 14:21:57 +0700

    hiuhihiuhiuhiuh

--- lembaga/admin.py
+++ lembaga/admin.py
@@ -5,3 +5,5 @@ from .models import *
 admin.site.register(PKK)
 admin.site.register(Posyandu)
 admin.site.register(BUMDES)
+admin.site.register(BPD)
+admin.site.register(KarangTaruna)
--- lembaga/models.py
+++ lembaga/models.py
@@ -7,6 +7,9 @@ class PKK(models.Model):
     jabatan = models.CharField(max_length=255, null=False, blank=False)
     alamat = models.CharField(max_length=1000, null=False, blank=False)
 
+    def __str__(self):
+        return self.nama_pengurus
+
     class Meta:
         verbose_name="PKK"
         verbose_name_plural = "PKK"
@@ -16,9 +19,23 @@ class Posyandu(models.Model):
     nama_posyandu = models.CharField(max_length=255, null=True, blank=True)
     alamat = models.CharField(max_length=1000, null=False, blank=False)
 
+    def __str__(self):
+        return self.nama
+
     class Meta:
         verbose_name="Posyandu"
         verbose_name_plural = "Posyandu"
+
+class BPD(models.Model):
+    nama = models.CharField(max_length=255, null=False, blank=False)
+    jabatan = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return self.nama
+
+    class Meta:
+        verbose_name="Pengurus"
+        verbose_name_plural = "BPD"
 
 class BUMDES(models.Model):
     nama_bumdes = models.CharField(max_length=255, null=False, blank=False)
@@ -26,6 +43,20 @@ class BUMDES(models.Model):
     struktur = models.ImageField(upload_to="struktur_bumdes/")
     foto_bumdes = models.ImageField(upload_to="bumdes/")
 
+    def __str__(self):
+        return self.nama_bumdes
+
     class Meta:
         verbose_name="BUMdes"
         verbose_name_plural = "BUMdes"
+
+class KarangTaruna(models.Model):
+    nama = models.CharField(max_length=255, null=False, blank=False)
+    jabatan = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return self.nama 
+
+    class Meta:
+        verbose_name="Pengurus"
+        verbose_name_plural = "Karang Taruna"
